39852316c78f313d4518cf6b45aa6024e0ea7c4f
John Isaac Delgado <ji23delgado@gmail.com>
2020-11-21 18:01:15 +0800

    add gitignore rules, improved file naming

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import subprocess
 
+#read the file number
+filenumber = open('filenumber.txt', 'r')
+fileNum = filenumber.readline()[-1]
+
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 wifis = [line.split(':')[1][1:-1] for line in data if "All User Profile" in line]
-f = open('Passwords.txt', 'w+')
+f = open('Passwords{0}.txt'.format(fileNum), 'w+')
+print(fileNum)
+filenumber.close()
+
+# add the number
+filenumber = open('filenumber.txt', 'a+')
+filenumber.write(str(int(fileNum) + 1))
+filenumber.close()
 
 for wifi in wifis:
     password = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', wifi, 'key=clear']).decode('utf-8').split('\n')
     password = [line.split(':')[1][1:-1] for line in password if "Key Content" in line]
     f.write('Wifi = {0}, Password = {1}\n'.format(wifi, password))
     print('Wifi = {0}, Password = {1}'.format(wifi, password))
+
+f.close()
